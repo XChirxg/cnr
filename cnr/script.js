d73cef79ff78523536d0039c52095899151f96dc
XChirxg <158815059+XChirxg@users.noreply.github.com>
2025-02-14 02:48:00 +0530

    Add files via upload

---
+++ cnr/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".cta").addEventListener("click", function() {
+    alert("Viewing Work...");
+});
+document.querySelector(".contact-btn").addEventListener("click", function() {
+    alert("Redirecting to Contact Form...");
+});
