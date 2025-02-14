5a2f8f0a73d2eda0f062740d5a8ca22cedd4a7e0
XChirxg <158815059+XChirxg@users.noreply.github.com>
2025-02-14 17:00:28 +0530

    Delete cnr directory

--- cnr/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".cta").addEventListener("click", function() {
-    alert("Viewing Work...");
-});
-document.querySelector(".contact-btn").addEventListener("click", function() {
-    alert("Redirecting to Contact Form...");
-});
